18471cedb951196e4539aac265d7bc3c1ff04d80
aiyer10 <anant.iyer124@gmail.com>
2022-03-30 10:13:48 +0100

    Added key to locations dataframe for merging later

--- cleanFiles.py
+++ cleanFiles.py
@@ -2,11 +2,12 @@ import pandas as pd, numpy as np
 import os, glob
 
 
-locations = pd.read_csv(r'C:\Users\IyerA3\OneDrive - Vodafone Group\Personal\CambridgeshireTraffic-main\CambridgeSmartSensors\Locations.csv')
-# print(locations.head())
+locations = pd.read_csv(r'data/Locations.csv')
+locations['SensorID'] = r'S'+locations['Sensor Reference'].astype(str)
+print(locations.head())
 
 d = pd.DataFrame()
-for f in glob.glob(os.path.join(r'C:\Users\IyerA3\OneDrive - Vodafone Group\Personal\CambridgeshireTraffic-main\CambridgeSmartSensors', 'S*.csv')):
+for f in glob.glob(os.path.join(r'data/', 'S*.csv')):
 	print(f.split('\\')[-1])
 	df = pd.read_csv(f, usecols=['Date','Time', 'countlineName', 'direction', 'Car', 'Pedestrian', 'Cyclist', 'Motorbike', 'Bus', 'OGV1','OGV2','LGV'], parse_dates=[['Date','Time']])
 	df['SensorID'] = df['countlineName'].str.extract(r'(S[^_]*)', expand=True)
@@ -21,5 +22,5 @@ for f in glob.glob(os.path.join(r'C:\Users\IyerA3\OneDrive - Vodafone Group\Pers
 	d = pd.concat([d, df1], axis=0)
 	print(df1.head())
 print(d['weekday'].unique())	
-d.to_parquet(r'C:\Users\IyerA3\OneDrive - Vodafone Group\Personal\CambridgeshireTraffic-main\CambridgeSmartSensors\SensorData.parquet', index=None, engine='fastparquet')
+d.to_parquet(r'results/SensorData.parquet', index=None, engine='fastparquet')
 # d.to_csv(r'C:\Users\IyerA3\OneDrive - Vodafone Group\Personal\CambridgeshireTraffic-main\CambridgeSmartSensors\SensorData.csv', index=None)
